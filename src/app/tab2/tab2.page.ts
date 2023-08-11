5121d590deed2cf98ce1a9dbd04a15787d423680
DiDrev <134726649+DiDrev@users.noreply.github.com>
2023-08-11 16:07:18 -0600

    Created a tabs app

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AlertController, ToastController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab2',
@@ -7,6 +8,45 @@ import { Component } from '@angular/core';
 })
 export class Tab2Page {
 
-  constructor() {}
+	public alertButtons = ['OK','Cancel','Love you'];
+
+  constructor(private alertController: AlertController, private toastController: ToastController) {}
+
+  async presentAlert() {
+	const alert = await this.alertController.create({
+	  header: 'Alert',
+	  message: 'This is an alert!',
+	  buttons: [
+		{
+			text: 'OK',
+			handler: () => {
+				console.log('OK button clicked');
+			}
+		},
+		{
+			text: 'DONT DO IT',
+			handler: () => {
+				console.log('I told you not to!');
+			}
+		}
+		],
+	});
+
+
+	
+	await alert.present();
+ }
+
+ async presentToast(position: 'top' | 'middle' | 'bottom') {
+	const toast = await this.toastController.create({
+	  message: 'Hello World!',
+	  duration: 300,
+	  position: position,
+	});
+
+	await toast.present();
+ }
 
 }
+
+
